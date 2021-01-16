6c28883145af522ae6ae6a7e20bb8a7f6cad6e0d
mswgen <54498184+mswgen@users.noreply.github.com>
2021-01-16 11:23:49 +0900

    First Commit

--- preload.js
+++ preload.js
@@ -1,12 +1,47 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+window.addEventListener('load', () => {
+  const axios = require('axios').default;
+  const punycode = require('punycode/');
+  document.querySelector('#resolve').addEventListener('click', () => {
+    if (document.querySelector('#server').value == 'cf') {
+      axios.get(`https://cloudflare-dns.com/dns-query?name=${punycode.toASCII(document.querySelector('#domain').value)}&type=${document.querySelector('#type').value}`, {
+        headers: {
+          accept: 'application/dns-json'
+        }
+      }).then(res => {
+        document.querySelector('#result').style.display = 'block';
+        if (res.data.Status != 0) {
+          document.querySelector('#success').innerHTML = '실패'
+          document.querySelector('#success').style.color = 'red';
+        } else {
+          document.querySelector('#success').innerHTML = '성공'
+          document.querySelector('#success').style.color = 'green';
+          document.querySelector('#answer').innerHTML = '';
+          for (let record of res.data.Answer) {
+            let elem = document.createElement('div');
+            elem.setAttribute('class', 'answer-card')
+            elem.innerHTML = `도메인: ${record.name}<br><br>TTL: ${record.TTL}초<br><br>결과: <strong>${record.data}</strong>`;
+            document.querySelector('#answer').appendChild(elem);
+          }
+        }
+      })
+    } else if (document.querySelector('#server').value == 'google') {
+      axios.get(`https://dns.google/resolve?name=${punycode.toASCII(document.querySelector('#domain').value)}&type=${document.querySelector('#type').value}`).then(res => {
+        document.querySelector('#result').style.display = 'block';
+        if (res.data.Status != 0) {
+          document.querySelector('#success').innerHTML = '실패'
+          document.querySelector('#success').style.color = 'red';
+        } else {
+          document.querySelector('#success').innerHTML = '성공'
+          document.querySelector('#success').style.color = 'green';
+          document.querySelector('#answer').innerHTML = '';
+          for (let record of res.data.Answer) {
+            let elem = document.createElement('div');
+            elem.setAttribute('class', 'answer-card')
+            elem.innerHTML = `도메인: ${record.name}<br><br>TTL: ${record.TTL}초<br><br>결과: <strong>${record.data}</strong>`;
+            document.querySelector('#answer').appendChild(elem);
+          }
+        }
+      })
+    }
+  })
+});
