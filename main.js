74efc15c67d0b9b5b283ff7297e674be37ad9f53
mswgen <54498184+mswgen@users.noreply.github.com>
2021-01-16 11:30:42 +0900

    자동 업데이트 구성

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const updater = require('update-electron-app');
 function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 1280,
@@ -12,10 +13,13 @@ function createWindow () {
 }
 app.whenReady().then(() => {
   createWindow()
+  if (process.platform == 'win32' || process.platform == 'darwin') {
+    updater();
+  }
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform != 'darwin') app.quit()
 })
