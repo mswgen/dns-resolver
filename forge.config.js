784be7dbc8bda71837e8fb5e68f4ffc827be5947
mswgen <54498184+mswgen@users.noreply.github.com>
2021-01-23 11:59:00 +0900

    msi 추가

--- forge.config.js
+++ forge.config.js
@@ -22,16 +22,30 @@ module.exports = {
             }
         },
         {
+            name: '@electron-forge/maker-wix',
+            config: {
+                description: 'A simple DNS Resolver app',
+                exe: 'dns-resolver.exe',
+                language: 1042,
+                manufacturer: 'Team int',
+                name: 'DNS Resolver',
+                ui: {
+                    chooseDirectory: true
+                }
+            }
+        },
+        {
             name: '@electron-forge/maker-squirrel',
             config: {
-                noMsi: true,
                 name: 'dns-resolver',
-                exe: 'dns-resolver.exe'
+                exe: 'dns-resolver.exe',
+                setupIcon: 'logo.ico'
             }
         }
     ],
     packagerConfig: {
         name: 'DNS Resolver',
-        executableName: 'dns-resolver'
+        executableName: 'dns-resolver',
+        icon: 'logo'
     }
 }
